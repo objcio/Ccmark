2d2002db33c3f077635538031008c6893a4c0ec2
Florian Kugler <mail@floriankugler.com>
2018-11-23 15:12:22 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
    name: "Ccmark",
    pkgConfig: "libcmark",
    providers: [
-       .Brew("commonmark")
+       .brew(["commonmark"])
    ]
 )
